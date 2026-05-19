569cab5951051825dbfd5f05a7498787cd33d152
Matheus Guilarducci <mguilarducci@gmail.com>
2026-05-18 21:39:51 -0300

    feat(cli): add cobra outdated subcommand

---
+++ internal/cli/outdated_cmd_test.go
@@ -0,0 +1,17 @@
+package cli
+
+import "testing"
+
+func TestOutdatedCmd_Registered(t *testing.T) {
+	t.Parallel()
+
+	found := false
+	for _, c := range rootCmd.Commands() {
+		if c.Use == "outdated" {
+			found = true
+		}
+	}
+	if !found {
+		t.Errorf("outdated subcommand not registered")
+	}
+}
