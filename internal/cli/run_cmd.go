db3e820fe79951f273a40f593398243ee7123b95
Matheus Guilarducci <mguilarducci@gmail.com>
2026-05-21 22:49:30 -0300

    feat(cli): wire run <name> command

---
+++ internal/cli/run_cmd.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/mguilarducci/liszt/internal/runner"
+)
+
+var runConfigPath string
+
+var runCmd = &cobra.Command{
+	Use:   "run <name>",
+	Short: "Run a named command group from .liszt/liszt.toml",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(_ *cobra.Command, args []string) error {
+		cfg, err := runner.Load(runConfigPath)
+		if err != nil {
+			return err
+		}
+		target, ok := cfg.Target(args[0])
+		if !ok {
+			return fmt.Errorf("no [run.%s] target in %s", args[0], runConfigPath)
+		}
+		os.Exit(target.Run(args[0], os.Stdout, os.Stderr))
+		return nil // coverage: unreachable, os.Exit terminates the process
+	},
+}
+
+func init() {
+	runCmd.Flags().StringVar(&runConfigPath, "config", ".liszt/liszt.toml", "config path")
+	rootCmd.AddCommand(runCmd)
+}
